28eaec25194781a888bb692dba9a65e3eb876005
jayo9901 <100823870+jayo9901@users.noreply.github.com>
2022-11-05 16:17:34 +0900

    인덱스,푸터

--- src/main/java/kr/co/study/bunjang/mvc/controller/pages/IndexController.java
+++ src/main/java/kr/co/study/bunjang/mvc/controller/pages/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-	
+
 	@RequestMapping(value = "/")
 	public String index() {
 		return "pages/index";
