1544a69cdedeb5e05c8a7d0294bde648866ffd47
Handonggon <gksehdrhs2@gmail.com>
2022-11-05 20:38:54 +0900

    내상점

--- src/main/java/kr/co/study/bunjang/mvc/controller/pages/ErrorController.java
+++ src/main/java/kr/co/study/bunjang/mvc/controller/pages/ErrorController.java
@@ -1,18 +1,16 @@
 package kr.co.study.bunjang.mvc.controller.pages;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import kr.co.study.bunjang.component.exception.AuthenticationEntryPointException;
 import kr.co.study.bunjang.component.utility.MessageUtils;
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,25 +23,9 @@ public class ErrorController extends AbstractErrorController {
         super(errorAttributes);
     }
 
-    @RequestMapping
-    public ModelAndView handleError(HttpServletRequest request, ModelAndView mav) {
-        mav.setViewName("/pages/error/error-404");
-
-        log.info("ErrorController - handleError");
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        mav.addObject("status", status);
-        if (status != null) {
-            int statusCode = Integer.valueOf(status.toString());
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return mav;
-            }
-        }
-        return mav;
-    }
-
-    @GetMapping(value = "/authentication")
-    public void entrypointException() {
-        throw new AuthenticationEntryPointException();
+    @RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView handleError(HttpServletRequest request) {
+        return new ModelAndView("redirect:/");
     }
 
     @GetMapping(value = "/accessdenied")
