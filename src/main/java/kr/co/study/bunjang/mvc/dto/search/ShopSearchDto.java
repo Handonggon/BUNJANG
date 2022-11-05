e45d5fd93333ae46a98955f8b8fe87e2ee7333b1
Handonggon <gksehdrhs2@gmail.com>
2022-11-05 20:53:48 +0900

    파일 정리

---
+++ src/main/java/kr/co/study/bunjang/mvc/dto/search/ShopSearchDto.java
@@ -0,0 +1,13 @@
+package kr.co.study.bunjang.mvc.dto.search;
+
+import kr.co.study.bunjang.mvc.dto.AbstractDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Builder
+@EqualsAndHashCode(callSuper = false)
+public class ShopSearchDto extends AbstractDto {
+
+}
