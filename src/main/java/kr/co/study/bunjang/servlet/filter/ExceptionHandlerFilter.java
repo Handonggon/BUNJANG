098020a6a64ce980c95052e6affaabd0e7c84f00
Handonggon <gksehdrhs2@gmail.com>
2023-01-15 19:05:53 +0900

    에러핸들링 필터

---
+++ src/main/java/kr/co/study/bunjang/servlet/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,23 @@
+package kr.co.study.bunjang.servlet.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            // API 에러 분기처리 및 리다이렉트 URL 수정
+            response.sendRedirect("/");
+        }
+    }
+}
